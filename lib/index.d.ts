0af96a2f83e097e1bbf61cf742ee397ef1a2d780
Amon De Shir <amonsir29@gmail.com>
2021-10-17 00:39:28 +0200

    Fix types
    index.d.ts
    Change some function return type from SoupElement to SoupTag to make test passed after change file type from js to ts.
    
    package.json
    Add cp command into the build script to copy index.d.ts into the dist/lib folder after build.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,20 +4,20 @@ declare module "jssoup" {
   }
 
   class SoupElement {
-    public parent?: SoupElement;
-    public previousElement?: SoupElement;
-    public nextElement?: SoupElement;
+    public parent?: SoupTag;
+    public previousElement?: SoupTag;
+    public nextElement?: SoupTag;
 
     constructor(
-      parent?: SoupElement,
-      previousElement?: SoupElement,
-      nextElement?: SoupElement
+      parent?: SoupTag,
+      previousElement?: SoupTag,
+      nextElement?: SoupTag
     );
 
-    public get nextSibling(): SoupElement | undefined;
-    public get previousSibling(): SoupElement | undefined;
-    public  get nextSiblings(): SoupElement | undefined;
-    public get previousSiblings(): SoupElement | undefined;
+    public get nextSibling(): SoupTag | undefined;
+    public get previousSibling(): SoupTag | undefined;
+    public get nextSiblings(): SoupTag | undefined;
+    public get previousSiblings(): SoupTag | undefined;
 
     /** remove item from dom tree */
     public extract(): void;
@@ -43,13 +43,13 @@ declare module "jssoup" {
     | { class: string[] }
     | { [attribute: string]: string };
 
-  class SoupTag extends SoupElement {
+  export class SoupTag extends SoupElement {
     public name: string;
     public builder: TreeBuilder;
     public attrs: { [attribute: string]: string };
 
     /** contains direct children of current element */
-    public contents: SoupElement[];
+    public contents: SoupTag[];
 
     constructor(
       name: string,
@@ -63,30 +63,53 @@ declare module "jssoup" {
     /** includes all elements of which current element is the ancestor of */
     public get descendants(): SoupElement[];
 
-    public find(name?: string, attrs?: Args, string?: string): SoupElement | undefined;
+    public find(
+      name?: string,
+      attrs?: Args,
+      string?: string
+    ): SoupTag | undefined;
+
     /** like find_all in BeautifulSoup */
-    public findAll(name?: string, attrs?: Args, string?: string): SoupElement[];
-    public findPreviousSibling(name?: string, attrs?: Args, string?: string): SoupElement | undefined;
-    public findPreviousSiblings(name?: string, attrs?: Args, string?: string): SoupElement[];
-    public findNextSibling(name?: string, attrs?: Args, string?: string): SoupElement | undefined;
-    public findNextSiblings(name?: string, attrs?: Args, string?: string): SoupElement[];
+    public findAll(name?: string, attrs?: Args, string?: string): SoupTag[];
+
+    public findPreviousSibling(
+      name?: string,
+      attrs?: Args,
+      string?: string
+    ): SoupTag | undefined;
+
+    public findPreviousSiblings(
+      name?: string,
+      attrs?: Args,
+      string?: string
+    ): SoupTag[];
+
+    public findNextSibling(
+      name?: string,
+      attrs?: Args,
+      string?: string
+    ): SoupTag | undefined;
+
+    public findNextSiblings(
+      name?: string,
+      attrs?: Args,
+      string?: string
+    ): SoupTag[];
+
     public getText(separator?: string): string;
-    
     public prettify(indent?: string, breakline?: string): string;
     public toString(): string;
-
     public append(item: SoupElement): void;
-    
-    /** @param expression - a CSS expression like "div > .class1"*/
-    public select(expression: string): SoupElement | undefined;;
 
     /** @param expression - a CSS expression like "div > .class1"*/
-    public selectOne(expression: string): SoupElement[];
+    public select(expression: string): SoupTag[];
+
+    /** @param expression - a CSS expression like "div > .class1"*/
+    public selectOne(expression: string): SoupTag | undefined;
   }
 
   export default class JSSoup extends SoupTag {
     /** The text element only contains whitespace will be ignored by default. To disable this feature, set "ignoreWhitespace" to true. */
     constructor(text: string, ignoreWhitespace?: boolean);
-    public hidden: boolean;
   }
 }
